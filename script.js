45835e1d390d31da3a90f5af25eed9acf88e1940
Delmy Arguello <kroarguelloadel@gmail.com>
2020-09-23 19:41:54 -0500

    Adding all question y validation for the password

--- script.js
+++ script.js
@@ -16,21 +16,41 @@ generatePassword();
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Asking if you want to continue and how many numbers 
+//Asking if you want to continue and how many numbers and if between 8 and 128
 
 function generatePassword(){
-   alert("hola 2");
-   if (confirm("Do you want to continue Generating Password") == true){
-     
-    var n = prompt( "How many numbers would you like ? (must to have between 8 to 128 characters)");
-    
+   
+  if (confirm("Do you want to continue Generating Password") == true){
+        var seguirNext=1;
+        for (seguirNext=1;seguirNext < 3;seguirNext++){
+           var sinum = prompt( "How many numbers would you like ? (must to have between 8 to 128 characters)");
+          if(sinum != null && sinum != 0 ){
+      
+              if (sinum >= 8 && sinum <= 128){
+                 alert("Your password will have " + sinum + " numbers");  
+                 var silower = confirm("Do you want lower case ?");
+                 var siupper = confirm("Do you want Upper case ?");
+                 var sispecial = confirm("Do you want Special characters ?");
+                 
+                 seguirNext = 3;
+              }
+              else{
+                 alert("Try again, should be a number between 8 and 128");
+                 seguirNext = 1;
+              }
 
+           }
+          else {
+           alert("Try again, should be a number between 8 and 128");
+           seguirNext = 1;
+           }
+       }
 
     }
    else {
      alert("Thank you see you next time");    
    }
-
+  
 }
 
 
